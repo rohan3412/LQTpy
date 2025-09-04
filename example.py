864c79d4101339f9f6ac625c960f0591485d25fb
rohan3412 <114685156+rohan3412@users.noreply.github.com>
2025-09-04 16:08:52 +0530

    Add files via upload

---
+++ example.py
@@ -0,0 +1,7 @@
+from LQTpy.LQTpy import LQTpy
+
+LQTpy('lesion.nii', atlas="Harvard_Oxford_test",output_path = "output/HO_atlas")
+
+LQTpy('lesion.nii', atlas="Freesurfer", output_path = "output/Freesurfer_atlas")
+
+
