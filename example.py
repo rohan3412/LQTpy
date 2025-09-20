2249217450e6c77b06f7a40194dc69caa5b39675
rohan3412 <114685156+rohan3412@users.noreply.github.com>
2025-09-20 15:52:14 +0530

    Update example.py

--- example.py
+++ example.py
@@ -1,7 +1,6 @@
 from LQTpy.LQTpy import LQTpy
 
-LQTpy('lesion.nii', atlas="Harvard_Oxford_test",output_path = "output/HO_atlas")
+LQTpy('lesion.nii',output_path = "output/HO_atlas")
 
-LQTpy('lesion.nii', atlas="Freesurfer", output_path = "output/Freesurfer_atlas")
 
 
